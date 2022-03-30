de6ec56dc3df4b464e41693428b9b193de779894
adrianasampaio91 <adrianadelimasampaio@gmail.com>
2022-03-30 12:17:50 +0200

    improvements in the js using existing classes from css file

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-const firstPage = document.querySelector(".firs-page");
-const secondPage = document.querySelector(".second-page");
-const inputName = document.querySelector(".input-name");
-const addName = document.querySelector(".add-name");
-const images = document.querySelector(".images");
+let inputName = document.querySelector(".input-name");
+let addName = document.querySelector(".add-name");
+let images = document.querySelector(".images");
+let img1 = document.querySelector(".img-1");
+let img2 = document.querySelector(".img-2");
 
+let imgBox = document.querySelector(".img-box");
 window.addEventListener("load", loadImg);
 
-const playerName = inputName.value;
-window.addEventListener("load", loadImg);
-addEventListener("click", chooseOption);
+let playerName = inputName.value;
+
 function loadImg() {
-  const url = `https://api.unsplash.com/photos/random?topics=travel&orientation=portrait&count=2&client_id=O46G6GwsSkoGM9IKuyb6mx8REyt_8Z9EPf7W9w12S70`;
+  const url = `https://api.unsplash.com/photos/random?topics=travel&orientation=portrait&client_id=O46G6GwsSkoGM9IKuyb6mx8REyt_8Z9EPf7W9w12S70`;
 
   //   &count=${numberOfPlayers}&client_id=
 
@@ -21,24 +21,19 @@ function loadImg() {
     .then((data) => {
       //   data.results.forEach((result) => {
       //   ADDING THE IMAGES
-      const imgBox = document.createElement("div");
-      imgBox.classList.add("img-box");
-      const img1 = document.createElement("img");
-      img1.src =
-        //   data.urls.full;
-        img1.classList.add("img-1");
+      img1.src = data.urls.full;
       img1.alt = data.alt_description;
-      const img2 = document.createElement("img");
-      img2.src =
-        //   data.urls.full;
-        img2.classList.add("img-2");
-      img2.alt = data.alt_description;
 
-      imgBox.appendChild(img1);
-      imgBox.appendChild(img2);
-      images.appendChild(imgBox);
+      img2.src = data.urls.full;
+      img2.alt = data.alt_description;
     });
   // });
 }
 
-function chooseOption() {}
+function chooseOption() {
+  if (img1) {
+    img2.remove();
+  }
+}
+
+imgBox.addEventListener("click", chooseOption);
