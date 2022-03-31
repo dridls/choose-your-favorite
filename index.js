18124743b53c00beb15468a0a0b5788f2980e1a5
adrianasampaio91 <adrianadelimasampaio@gmail.com>
2022-03-31 10:57:20 +0200

    add object with images and giving the city name as feedback in the test

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+class VacationDestiny {
+  constructor(id, img, city, country) {
+    this.id = id;
+    this.img = img;
+    this.city = city;
+    this.country = country;
+  }
+}
+let destinationArray = [];
+
 let startBtn = document.querySelector(".start-btn");
 let images = document.querySelector(".images");
 let inputNumber = document.querySelector(".input-number");
@@ -7,12 +17,14 @@ let secondPage = document.querySelector(".second-page");
 secondPage.classList.add("hidden");
 startBtn.addEventListener("click", loadImg);
 let message = document.querySelector(".message");
+
 function loadImg() {
   function showHide() {
     firstPage.classList.add("hidden");
     secondPage.classList.remove("hidden");
   }
   showHide();
+
   const URL = `https://api.unsplash.com/photos/random?collections=FuUX4SKtTZs&client_id=E5Gk22D-ZhNqpgKivS-KAHFcI1iSsq1X4rSa6NqXclo&count=${inputNumber.value}`;
 
   fetch(URL)
@@ -30,17 +42,27 @@ function loadImg() {
         image.alt = imageData.alt_description;
         imageBox.appendChild(image);
         images.appendChild(imageBox);
+        const destination = new VacationDestiny(
+          imageData.id,
+          imageData.urls.regular,
+          imageData.location.city,
+          imageData.location.country
+        );
+        destinationArray.push(destination);
+        console.log(destinationArray);
 
         function addMessage() {
-          message.textContent = "This is how you should enjoy your vacation!";
-          // let message = document.createElement("p");
-          // message.textContent += `This is how you should enjoy your vacations!`;
-          // messageBox.appendChild(message);
+          if (destination.city === null) {
+            message.textContent = `You should go to ${destination.country}!`;
+          } else {
+            message.textContent = `You should go to ${destination.city}, ${destination.country}!`;
+          }
         }
 
         function deleteImg() {
           const allImages = document.querySelectorAll(".img-box");
           if (allImages.length < 2) return;
+
           if (allImages[0].getAttribute("id") === imageData.id) {
             allImages[1].remove();
           } else {
